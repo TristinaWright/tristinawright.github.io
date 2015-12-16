9c60c1118abd1d12c7da23988a9fa2cf77be024c
Stephen <swright987@gmail.com>
2015-12-16 09:52:25 -0500

    update site speed sample rate

--- site.js
+++ site.js
@@ -6,5 +6,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-36022425-1', 'auto');
+ga('create', 'UA-36022425-1', {'siteSpeedSampleRate': 100});
 ga('send', 'pageview');
